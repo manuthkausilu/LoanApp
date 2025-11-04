5b35aca4064ed22b9cf3b69b261f24fa317f1d59
ASUS <manuthkausilu20031018@gmail.com>
2025-11-04 19:04:50 +0530

    all working done

--- service/superbase.ts
+++ service/superbase.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
 const SUPABASE_URL = 'https://fnmjbdvijyyvvoedlyus.supabase.co';
@@ -11,7 +12,7 @@ export const supabase = createClient(
   SUPABASE_ANON_KEY,
   {
     auth: {
-      storage: AsyncStorage,
+      storage: Platform.OS === 'web' ? undefined : AsyncStorage,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
